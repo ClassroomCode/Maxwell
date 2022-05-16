9f37b77d15055b2af281e86889c28ad2353c2092
Jason Bell <jbell@accelebrate.com>
2022-05-16 07:54:39 -0700

    LINQ 1

---
+++ LinqApp/LinqApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var evens = nums.Where(i => i % 2 == 0);
+
+            foreach (var num in evens) {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("----------");
+
+            nums.Add(12);
+
+            foreach (var num in evens) {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
